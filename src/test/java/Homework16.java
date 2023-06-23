a4dd9dff306bdfd592e5f5c8d6d739d9da93fde2
vefflores88 <victor.flores@testpro.io>
2023-06-23 08:30:48 -0700

    Create Homework16.java

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework16 extends BaseTest{
+    public void registrationNavigation() {
+
+        //Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //navigate to URL
+        String url = "https://qa.koel.app/";
+
+        //click on registration link
+        WebElement registration = driver.findElement(By.cssSelector("[id = 'hel']"));
+        registration.click();
+
+        //check URL
+        String checkUrl = "https://qa.koel.app/registration.php";
+        Assert.assertEquals(driver.getCurrentUrl(), checkUrl);
+        driver.quit();
+    }
+}
