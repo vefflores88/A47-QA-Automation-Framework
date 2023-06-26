0e6acad167badd0f1c373994b42af25c1d69ead3
vefflores88 <victor.flores@testpro.io>
2023-06-26 15:54:03 -0700

    working

---
+++ src/test/java/Homework17.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class Homework17 {
+
+    public void addSongToPlaylist(){
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+
+    }
+
+
+
+
+}
