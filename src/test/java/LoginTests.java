3f55d75facb04bcdc5aff97fe3980bb7c4b53e75
vefflores88 <victor.flores@testpro.io>
2023-07-01 12:25:57 -0700

    progress

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -10,18 +10,10 @@ public class LoginTests extends BaseTest {
     @Test (dataProvider = "IncorrectLoginProviders")
     public void LoginEmptyEmailPasswordTest(String email, String password) {
 
-        public void LoginEmptyEmailPasswordTest() {
+        enterEmail(email);
+        enterEmail(email);
+        enterPassword(password);
 
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url = "https://qa.koel.app/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+        clickSubmit();
     }
 }
